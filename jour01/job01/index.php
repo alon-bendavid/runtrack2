1756155def02f9d42f755c1352a5975a9ba865ad
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-14 16:18:41 +0100

    fisrt two jobs

---
+++ jour01/job01/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    $str = "laplateforme";
+
+    $str2 = "vive";
+    $str3 = "!";
+    // echo $str;
+    // echo "$str2 $str $str3";
+
+    $val = 6 + 4;
+    // echo $val;
+
+    $myBool = false;
+
+    echo $myBool;
+
+
+
+    // echo "Hello LaPlateforme!"; //one line comment 
+
+
+    // echo "Hello LaPlateforme!"; /*milti line comment
+    // multiline*/
+
+
+
+
+    ?>
+
+
+
+</body>
+
+</html>
