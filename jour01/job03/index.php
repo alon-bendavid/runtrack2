c750257d6c3183436685673cb0f1e74dca1ec68a
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-15 13:34:33 +0100

    day two first job

---
+++ jour01/job03/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="css\style.css"> -->
+</head>
+
+<body>
+    <?php
+    $type = "string";
+
+
+    ?>
+    <table>
+
+        <caption>Comparatif des formules d'abonnement</caption>
+
+        <tr>
+            <th><?php echo $type ?></th>
+            <th> Durée</th>
+            <th>Prix</th>
+        </tr>
+
+
+        <tr>
+            <td>Chaton</td>
+            <td>2 mois</td>
+            <td>40 €</td>
+        </tr>
+
+        <tr>
+            <td>Elan</td>
+            <td>6 mois</td>
+            <td>110 €</td>
+        </tr>
+
+        <tr>
+            <td>Licorne</td>
+            <td>12 mois</td>
+            <td>200 €</td>
+        </tr>
+
+    </table>
+
+
+
+</body>
+
+</html>
---
+++ jour02/job01/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // decler number var
+
+    for ($x = 0; $x - 1 < 1337; $x++) {
+        if ($x === 42) {
+            echo "<b><u>$x</u></b><br/>";
+        } else {
+            echo "$x<br/> ";
+        }
+
+
+
+
+
+        // if ($x === 42) {
+        //     echo "$x<br/>";
+        // } else ($x === 42) {
+        //     echo "<b><u>$x</u></b>";
+        // }
+    }
+
+    ?>
+
+</body>
+
+</html>
+<!-- if ($x === 42) {
+            echo " <b>$x<u/>";
+        } else {
+            echo "<br>$x<br/> "; -->
+
+
+<!-- if ($x === 42) {
+            echo " <b><u>$x<br/>";
+        } else {
+            echo "$x<br/> ";
+        } -->
