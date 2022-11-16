8fc9bd8afc4b82d3e98f4fd9f955a59bb0b899eb
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-16 11:01:55 +0100

    day 03 first job

---
+++ jour03/job01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $table = array(200, 204, 173, 98, 171, 404, 459);
+
+    foreach ($table as $value) {
+        if ($value % 2 == 0) {
+            echo $value . " est paire"    . "</br>";
+        } else {
+            echo $value . " est impaire" . "</br>";
+        }
+    }
+
+
+    ?>
+</body>
+
+</html>
