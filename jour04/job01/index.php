1656e6f28298419c3f74d9a2251b0c76faa4e315
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-17 12:22:27 +0100

    day 04 job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <style>
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 55%;
+            height: 9rem;
+            align-items: center;
+            background-color: grey;
+            justify-content: space-around;
+            border-radius: 15px;
+
+
+        }
+
+        body {
+            display: flex;
+
+            flex-direction: column;
+            align-items: center;
+            padding: 10%;
+        }
+    </style> -->
+</head>
+
+
+<body>
+    <?php
+    $count = 0;
+    //count the number of boxes
+    foreach ($_GET as $i) {
+
+        $count++;
+    }
+    echo $count;
+
+    ?>
+
+    <form action="" method="get">
+
+        <input type="text" id="name" name="name">
+        <input type="text" id="age" name="age">
+        <button type="submit">Send -></button>
+    </form>
+
+
+</body>
+
+</html>
