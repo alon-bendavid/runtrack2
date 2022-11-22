33d5d5ab5dfde4db6d153c6ab124971d390c7121
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-22 12:21:47 +0100

    day07 job04

---
+++ jour07/job04/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <?php
+        function calcule($a, $operation, $b)
+        {
+
+            $resualt = 0;
+            switch ($operation) {
+                case "+":
+                    $resualt = $a + $b;
+                    break;
+                case "-";
+                    $resualt = $a - $b;
+                    break;
+                case "*";
+                    $resualt = $a * $b;
+                    break;
+                case "/";
+                    $resualt = $a / $b;
+                    break;
+                case "%";
+                    $resualt = $a % $b;
+                    break;
+                default:
+                    $resualt = "eror";
+            }
+            return $resualt;
+        }
+
+        echo calcule(70, "*", 5);
+
+
+        ?>
+
+    </main>
+</body>
+
+</html>
